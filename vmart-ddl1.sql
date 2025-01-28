a9d9d1daa98701ca84e5280e0776e501720e68c6
Long Nguyen <longnewint@gmail.com>
2025-01-27 20:46:06 -0500

    Add category view

--- vmart-ddl1.sql
+++ vmart-ddl1.sql
@@ -16,3 +16,19 @@ SET default_with_oids = false;
 --
 --
 --
+-- CREATE VIEW category_view AS
+WITH RECURSIVE category_cte AS
+(SELECT
+  category_id,
+  parent_category_id,
+  category_name
+FROM product_category
+UNION ALL
+SELECT
+  category.category_id,
+  category.parent_category_id,
+  category.category_name
+FROM category_cte
+JOIN product_category category
+ON category_cte.category_id = category.parent_category_id)
+SELECT * FROM category_cte
