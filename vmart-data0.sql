836707d7037cb92d9072c8d43ce130221c7b65df
Long Nguyen <longnewint@gmail.com>
2025-02-01 23:12:07 -0500

    Add vmart_user data

--- vmart-data0.sql
+++ vmart-data0.sql
@@ -15,6 +15,9 @@ SET default_with_oids = false;
 SET default_with_oids = false;
 SET SESSION CHARACTERISTICS AS TRANSACTION ISOLATION LEVEL SERIALIZABLE;
 
+INSERT INTO vmart_user VALUES
+(12345, 'dev@mail.com');
+
 INSERT INTO store VALUES
 (nextval('store_seq'), 'VMart Ottawa', '6134131234'),
 (nextval('store_seq'), 'VMart Kanata', '6134132345');
