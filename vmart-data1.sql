9f326d4afa30f3fbfd814edca70ca74fe52f8947
Long Nguyen <longnewint@gmail.com>
2025-02-10 22:30:04 -0500

    Add address and payment data dump

--- vmart-data1.sql
+++ vmart-data1.sql
@@ -14,3 +14,9 @@ SET default_with_oids = false;
 
 SET default_with_oids = false;
 SET SESSION CHARACTERISTICS AS TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+
+SELECT add_address(12345, NULL, 1227, 'Woodroffe Ave', NULL, 'K2C2T2', 1, 1, TRUE);
+SELECT add_address(12345, NULL, 1221, 'Woodroffe Ave', NULL, 'K2C2T2', 1, 1, FALSE);
+
+SELECT add_payment(12345, 1, '1234567812345678', '01', '27', '123', TRUE);
+SELECT add_payment(12345, 2, '1234123412341234', '01', '27', '123', TRUE);
