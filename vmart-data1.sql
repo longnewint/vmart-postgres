4333e371e4bad3088ccfe1d6a431d642ba728f9e
Long Nguyen <longnewint@gmail.com>
2025-02-12 23:16:25 -0500

    Use DEFAULT instead of NULL

--- vmart-data1.sql
+++ vmart-data1.sql
@@ -15,10 +15,10 @@ SET default_with_oids = false;
 SET default_with_oids = false;
 SET SESSION CHARACTERISTICS AS TRANSACTION ISOLATION LEVEL SERIALIZABLE;
 
-SELECT add_address(12345, NULL, 1227, 'Woodroffe Ave', NULL, 'K2C2T2', 1, 1, TRUE);
-SELECT add_address(12345, NULL, 1221, 'Woodroffe Ave', NULL, 'K2C2T2', 1, 1, FALSE);
+SELECT add_address(12345, DEFAULT, '1227', 'Woodroffe Ave', DEFAULT, 'K2C2T2', 1, 1, TRUE);
+SELECT add_address(12345, DEFAULT, '1221', 'Woodroffe Ave', DEFAULT, 'K2C2T2', 1, 1, FALSE);
 
 SELECT add_payment(12345, 1, '1234567812345678', '1', '27', '123', TRUE);
-SELECT add_payment(12345, 2, '1234123412341234', '1', '27', '123', TRUE);
+SELECT add_payment(12345, 2, '1234123412341234', '1', '27', '123', FALSE);
 
 SELECT add_to_cart();
