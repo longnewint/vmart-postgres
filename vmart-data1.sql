b674d5f0e1a33c65aeeef08dff772e674e4a9ebd
Long Nguyen <longnewint@gmail.com>
2025-02-21 00:22:57 -0500

    Add update_profile call

--- vmart-data1.sql
+++ vmart-data1.sql
@@ -26,6 +26,8 @@ INSERT INTO vmart_user VALUES
 INSERT INTO shopping_cart VALUES
 (1234567, 12345, 101);
 
+CALL update_profile(12345, 'Long Nguyen', '6134133792');
+
 CALL add_address(11111, TRUE, 'pick up', 'at the store', 'K2C2T2', 1, 1);
 CALL add_address(12345, TRUE, '1227', 'Woodroffe Ave', 'K2C2T2', 1, 1);
 CALL add_address(12345, FALSE, '1221', 'Woodroffe Ave', 'K2C2T2', 1, 1);
