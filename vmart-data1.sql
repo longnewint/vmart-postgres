78af1c5a0f4cdf19e5a1552a7213925c7bec6643
Long Nguyen <longnewint@gmail.com>
2025-02-25 14:47:36 -0500

    Change default user data

--- vmart-data1.sql
+++ vmart-data1.sql
@@ -28,20 +28,21 @@ INSERT INTO shopping_cart VALUES
 (1234567, 12345, 101),
 (1234321, 12321, 101);
 
-CALL update_profile(12345, 'Long Nguyen', '6134133792');
+CALL update_profile(12321, 'Test User', '6132160803');
 
 CALL add_address(11111, TRUE, 'none', 'pick up', 'at the store', 'none', 'Ottawa', 'Ontario', 'K2C2T2');
-CALL add_address(12345, TRUE, 'none', '1227', 'Woodroffe Ave', 'none', 'Ottawa', 'Ontario', 'K2C2T2');
-CALL add_address(12345, FALSE, 'none', '1221', 'Woodroffe Ave', 'none', 'Ottawa', 'Ontario', 'K2C2T2');
+CALL add_address(12321, TRUE, 'none', '1227', 'Woodroffe Ave', 'none', 'Ottawa', 'Ontario', 'K2C2T2');
+CALL add_address(12321, FALSE, 'none', '1221', 'Woodroffe Ave', 'none', 'Ottawa', 'Ontario', 'K2C2T2');
 
-CALL add_payment(12345, 1, '1234567812345678', '01', '27', '123', TRUE);
-CALL add_payment(12345, 2, '1234123412341234', '01', '27', '123', FALSE);
+CALL add_payment(11111, 1, '1111111111111111', '01', '01', '111', TRUE);
+CALL add_payment(12321, 1, '1234567812345678', '01', '27', '123', TRUE);
+CALL add_payment(12321, 2, '1234123412341234', '01', '27', '123', FALSE);
 
-CALL add_to_cart(1234567, 1002, 1);
-CALL add_to_cart(1234567, 1005, 1);
-CALL add_to_cart(1234567, 1007, 1);
-CALL add_to_cart(1234567, 1072, 1);
-CALL add_to_cart(1234567, 1038, 1);
-CALL add_to_cart(1234567, 1047, 1);
+CALL add_to_cart(1234321, 1002, 1);
+CALL add_to_cart(1234321, 1005, 1);
+CALL add_to_cart(1234321, 1007, 1);
+CALL add_to_cart(1234321, 1072, 1);
+CALL add_to_cart(1234321, 1038, 1);
+CALL add_to_cart(1234321, 1047, 1);
 
-CALL create_order(12345, 101, 1234567, 2, 2, 1);
+CALL create_order(12321, 101, 1234321, 2, 2, 2);
