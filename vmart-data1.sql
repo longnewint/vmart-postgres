e9091aa594b6fa9db49e8c201aeca8ae53a8f046
Long Nguyen <longnewint@gmail.com>
2025-02-25 15:30:22 -0500

    Change default user data2

--- vmart-data1.sql
+++ vmart-data1.sql
@@ -30,9 +30,9 @@ INSERT INTO shopping_cart VALUES
 
 CALL update_profile(12321, 'Test User', '6132160803');
 
-CALL add_address(11111, TRUE, 'none', 'pick up', 'at the store', 'none', 'Ottawa', 'Ontario', 'K2C2T2');
-CALL add_address(12321, TRUE, 'none', '1227', 'Woodroffe Ave', 'none', 'Ottawa', 'Ontario', 'K2C2T2');
-CALL add_address(12321, FALSE, 'none', '1221', 'Woodroffe Ave', 'none', 'Ottawa', 'Ontario', 'K2C2T2');
+CALL add_address(11111, TRUE, 'none', 'pick up', 'at the store', 'none', 'Ottawa', 'ON', 'K2C2T2');
+CALL add_address(12321, TRUE, 'none', '1279', 'Woodroffe Ave', 'none', 'Ottawa', 'ON', 'K2C2T7');
+CALL add_address(12321, FALSE, 'none', '1277', 'Woodroffe Ave', 'none', 'Ottawa', 'ON', 'K2C2T7');
 
 CALL add_payment(11111, 1, '1111111111111111', '01', '01', '111', TRUE);
 CALL add_payment(12321, 1, '1234567812345678', '01', '27', '123', TRUE);
