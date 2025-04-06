1b347f5124baed523c10497f196c8d4d548b53f9
Long Nguyen <longnewint@gmail.com>
2025-04-06 15:15:07 -0400

    Add comments to product ddl

--- product/vmart-product-ddl.sql
+++ product/vmart-product-ddl.sql
@@ -1,4 +1,24 @@
-CREATE VIEW product_view AS
+--
+-- vmart product view and functions
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET default_tablespace = '';
+SET default_with_oids = false;
+
+--
+--
+--
+
+-- Create product view
+
+CREATE OR REPLACE VIEW product_view AS
 SELECT
 	si.item_id,
 	si.store_id,
