803495c00768b56eaffaf1cd2ac2af16ac357c58
Long Nguyen <longnewint@gmail.com>
2025-04-06 15:24:38 -0400

    Add category function to helper ddl

---
+++ helper/vmart-helper-ddl.sql
@@ -0,0 +1,49 @@
+--
+-- vmart helper functions
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET default_tablespace = '';
+SET default_with_oids = false;
+
+--
+--
+--
+
+
+-- Category function
+
+CREATE FUNCTION get_category(parent_id integer)
+  RETURNS TABLE(
+    category_id integer,
+    category_name varchar(64))
+  LANGUAGE SQL
+AS
+$$
+WITH RECURSIVE category_cte AS
+(SELECT
+  category_id,
+  category_name,
+  is_leaf
+FROM product_category
+WHERE category_id = parent_id
+UNION ALL
+SELECT
+  category.category_id,
+  category.category_name,
+  category.is_leaf
+FROM category_cte
+JOIN product_category category
+ON category_cte.category_id = category.parent_category_id)
+SELECT
+  category_id,
+  category_name
+FROM category_cte
+WHERE is_leaf = TRUE;
+$$;
