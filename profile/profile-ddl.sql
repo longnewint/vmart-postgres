ab3fd8ae569cac4a70ef3f25ed1f8f548160a4d8
Long Nguyen <longnewint@gmail.com>
2025-04-09 22:51:31 -0400

    Reformat profile ddl

--- profile/profile-ddl.sql
+++ profile/profile-ddl.sql
@@ -1,6 +1,4 @@
 --
--- Profile function
---
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -12,8 +10,6 @@ SET client_min_messages = warning;
 SET default_tablespace = '';
 SET default_with_oids = false;
 
---
---
 --
 
 --
