83d80579aa0c48c14057f5e016f8e504c7caaba5
Long Nguyen <longnewint@gmail.com>
2025-04-09 22:54:55 -0400

    Reformat payment ddl

--- order/payment-ddl.sql
+++ order/payment-ddl.sql
@@ -1,3 +1,16 @@
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET default_tablespace = '';
+SET default_with_oids = false;
+
+--
 
 -- Payment function
 CREATE FUNCTION get_payment(customer_id integer)
