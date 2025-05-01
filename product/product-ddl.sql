338bc27e5d92cafde006fbac328a35736066f5ee
Long Nguyen <longnewint@gmail.com>
2025-05-01 14:12:46 -0400

    Move product view to ddl1

--- product/product-ddl.sql
+++ product/product-ddl.sql
@@ -10,17 +10,6 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 --
 
--- Create product view
-CREATE OR REPLACE VIEW product_view AS
-SELECT
-	si.item_id,
-	si.store_id,
-	si.discount_price,
-	pr.*
-FROM store_item AS si
-JOIN product AS pr
-ON si.product_id = pr.product_id
-
 -- Product function
 CREATE FUNCTION get_product_by_category(
 	parent_category_id integer,
--- vmart-ddl1-product.sql
+++ vmart-ddl1-product.sql
@@ -10,6 +10,17 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 --
 
+-- Create product view
+CREATE OR REPLACE VIEW product_view AS
+SELECT
+	si.item_id,
+	si.store_id,
+	si.discount_price,
+	pr.*
+FROM store_item AS si
+JOIN product AS pr
+ON si.product_id = pr.product_id
+
 -- Category function
 CREATE FUNCTION get_category(parent_id integer)
 RETURNS TABLE(
